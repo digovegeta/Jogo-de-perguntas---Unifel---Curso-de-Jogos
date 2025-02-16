10ad0e554ed21c01a14e21c482bec1e35e6d2bc0
Diogo Cardoso <digovegeta@gmail.com>
2025-02-16 18:16:59 -0300

    Ajuste das respostas corretas

--- JogoDePerguntas.cs
+++ JogoDePerguntas.cs
@@ -16,15 +16,15 @@ public class JogoDePerguntas{
     private void montarPerguntas(){
         perguntas = new Dictionary<string, Pergunta>();
         perguntas.Add("1", new Pergunta("Qual comando utilizado para escrever no terminal?", "Console.WriteLine", "Console.ReadLine", "Console.LineWrite", "Console.LineRead"));
-        perguntas.Add("2", new Pergunta("O C# é uma linguagem de qual categoria?", "Estruturada", "Orientada a Objetos", "Script", "Marcação"));
+        perguntas.Add("2", new Pergunta("O C# é uma linguagem de qual categoria?", "Orientada a Objetos", "Estruturada", "Script", "Marcação"));
         perguntas.Add("3", new Pergunta("O C# tem como pai qual outra linguagem?", "Java", "Python", "HTML", "C"));
         perguntas.Add("4", new Pergunta("Qual comando para criar comentários no C#?", "//", "/*", "||", "!--"));
-        perguntas.Add("5", new Pergunta("Dentre os tipos de variáveis abaixo, qual consome 1 bit?", " int", "char", "bool", "string"));
-        perguntas.Add("6", new Pergunta("Qual operador corresponde ao resto da divisão?", "# ", " $", "&", "%"));
+        perguntas.Add("5", new Pergunta("Dentre os tipos de variáveis abaixo, qual consome 1 bit?", "bool", " int", "char", "string"));
+        perguntas.Add("6", new Pergunta("Qual operador corresponde ao resto da divisão?", "%", "# ", " $", "&"));
         perguntas.Add("7", new Pergunta("Qual comando utilizamos para converter para inteiro?", "int.Parse ", "int.Convert ", "Convert.Parse ", "Convert.Int"));
-        perguntas.Add("8", new Pergunta("Qual símbolo usamos para escrever o valor da variável no terminal?","#", "%", "$", "&"));
-        perguntas.Add("9", new Pergunta("Qual dos nomes de variáveis abaixo é válido?", "123var", "nome variavel", "!nome!", "Variavel_123_Ex"));
-        perguntas.Add("10", new Pergunta("Qual comando para gerar números aleatórios?", "Console.Random", "Math.Random", "Random", "Random.Round"));
+        perguntas.Add("8", new Pergunta("Qual símbolo usamos para escrever o valor da variável no terminal?","$","#", "%",  "&"));
+        perguntas.Add("9", new Pergunta("Qual dos nomes de variáveis abaixo é válido?", "Variavel_123_Ex", "123var", "nome variavel", "!nome!"));
+        perguntas.Add("10", new Pergunta("Qual comando para gerar números aleatórios?", "Random", "Console.Random", "Math.Random", "Random.Round"));
         getPergunta();    
     }
     private void getPergunta(){
