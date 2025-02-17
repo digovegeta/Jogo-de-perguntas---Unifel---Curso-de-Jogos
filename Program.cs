9be3cb6ff0f523240409a77a224cbafce3094f63
Diogo Cardoso <digovegeta@gmail.com>
2025-02-16 21:29:26 -0300

    Inicializador dos jogos

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        new JogoDePerguntas();
+        Console.WriteLine("Qual jogo deseja sogar?");
+        Console.WriteLine("1 - Jogo da Velha");
+        Console.WriteLine("2 - Jogo de Perguntas");
+        int op = 0;
+        string opcao = Console.ReadLine();
+        if(int.TryParse(opcao, out op))
+            op = int.Parse(opcao);
         
+        switch(op) {
+            case 1:
+                new JogoDaVelha();
+                break;
+            case 2:
+                new JogoDePerguntas();
+                break;
+            default:
+                Console.WriteLine("Opção inválida!");
+                break;
+        }
     }
 }
